075000cfcb454f0748bab9a8cae8bfa8c2c78ea8
desmond-dong <djhui@live.cn>
2025-06-14 15:20:05 +0800

    修改文档宽度

---
+++ .rspress/theme/index.tsx
@@ -0,0 +1,10 @@
+// .rspress/theme/index.tsx
+import type { Theme } from 'rspress/theme';
+import DefaultTheme from 'rspress/theme-default';
+import './global.css';
+
+const theme: Theme = {
+  ...DefaultTheme,
+};
+
+export default theme;
