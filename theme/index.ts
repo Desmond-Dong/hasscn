76dd14079b5319b86db2155e5becb42bda744672
desmond-dong <djhui@live.cn>
2025-06-26 23:52:44 +0800

    更新主题

--- theme/index.ts
+++ theme/index.ts
@@ -1,2 +1,10 @@
-import './custom.css';        // 自定义样式
-export * from 'rspress/theme'; // 导出默认主题的全部命名导出
+import * as DefaultTheme from 'rspress/theme';  // 全量引入命名导出
+import './custom.css';
+
+// 继续导出命名导出（可选）
+export * from 'rspress/theme';
+
+// 补上默认导出（这是关键）
+export default {
+  ...DefaultTheme,
+};
